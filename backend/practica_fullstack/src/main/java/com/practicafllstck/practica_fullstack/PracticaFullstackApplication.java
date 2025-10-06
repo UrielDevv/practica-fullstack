c033edb129c3ddc79fbcc0d13d5203ac364ac9ca
Uriel Hernandez <uriel22h@gmail.com>
2025-10-06 15:28:53 -0600

    Anexo de Spring al proyecto y dependencias

---
+++ backend/practica_fullstack/src/main/java/com/practicafllstck/practica_fullstack/PracticaFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.practicafllstck.practica_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaFullstackApplication.class, args);
+	}
+
+}
