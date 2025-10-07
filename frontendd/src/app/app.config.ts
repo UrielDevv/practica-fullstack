77e0adecf6a16e0b2689df3db65ddfae7ef4a09f
Uriel Hernandez <uriel22h@gmail.com>
2025-10-07 11:27:04 -0600

    Integracion inicial de frontend con angular, login con auth, guard,dashboard, service

---
+++ frontendd/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { FormsModule } from '@angular/forms'; //Importado 
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(FormsModule) //Importado
+  ]
+};
