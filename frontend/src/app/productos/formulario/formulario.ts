a2753cff8a7ad4d963c064404f3f4a18a7cdc09a
Uriel Hernandez <uriel22h@gmail.com>
2025-10-09 14:46:29 -0600

    Se eliminaron comentarios

--- frontend/src/app/productos/formulario/formulario.ts
+++ frontend/src/app/productos/formulario/formulario.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductoService, Producto } from '../producto';
-import { log } from 'node:console';
 
 
 @Component({
@@ -30,17 +29,13 @@ export class FormularioProductoComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
-    // Lógica para cargar datos si es un formulario de edición
   }
 
   guardarProducto(): void {
     if (this.productoForm.invalid) {
       this.productoForm.markAllAsTouched(); // Marca todos los campos como "tocados" para mostrar errores
-      console.log('Formulario inválido:', this.productoForm);
       return;
     }
-    console.log('Datos del formulario:', this.productoForm.value);
     this.productoService.crearProducto(this.productoForm.value).subscribe(
       response => {
         console.log('Producto creado:', response);
@@ -53,7 +48,6 @@ export class FormularioProductoComponent implements OnInit {
     );
   }
 
-  // Pequeña función de ayuda para acceder a los controles en el HTML
   get f() {
     return this.productoForm.controls;
   }
